6e5e3948a2bf9e0cbebbd115219b37a8cbf04e7b
turtlembo <shooter27092004@gmail.com>
2025-08-24 16:38:02 +0300

    add checked users

--- script.js
+++ script.js
@@ -6,6 +6,13 @@ const emailError = document.querySelector("#email-error");
 const passwordError = document.querySelector("#password-error");
 const togglePasswordVisibility = document.querySelector("#toggle-password-visibility");
 
+const users = [
+    {email: 'user1@mail.ru', password:'Domr4532'},
+    {email: 'user1@mail.ru', password:'Domr4532'},
+    {email: 'user2@mail.ru', password:'Domr3332'},
+    {email: 'user3@mail.ru', password:'Domr32312'},
+]
+
 const validationRules = {
     emailRegex: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
     passwordRegex: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{4,}$/,
@@ -34,8 +41,23 @@ authForm.addEventListener('submit', (e)=>{
     const formObj = new FormData(e.target);
     const formData = Object.fromEntries(formObj);
     if (formData.email.match(validationRules.emailRegex) && formData.password.match(validationRules.passwordRegex)){
-        console.log("yes");
+    //     try {
+    //         users.forEach( (user)=>{
+    //         if(formData.email === user.email && formData.password === user.password){
+    //         console.log("User found");
+    //         throw new Error();
+    //         }
+    //     })
+    //     } catch (error) {
+
+    //  }
+        const isUser = users.find((user)=> formData.email === user.email && formData.password === user.password
+        )
+        console.log(isUser);
     }
+    
+    
+
 })
 authFormEmail.addEventListener('input', (e)=>{
     if (e.target.value.match(validationRules.emailRegex)){
